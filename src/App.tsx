c9008065b3ae4e0e4d54b718dfa2c59c2d7f87c7
HiBixby <201910789@sangmyung.kr>
2023-03-15 15:15:28 +0900

    💄  메인페이지 링크 버튼 모양으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,27 @@ import styled from "styled-components";
 const S = {
   LinkList: styled.ul`
     list-style: none;
+    margin: 0;
+    padding: 0;
   `,
-  LinkItem: styled.li`
-    &:before {
-      content: "✨ ";
+  LinkItem: styled.li``,
+  Link: styled(Link)`
+    display: block;
+    margin: 1rem;
+    padding: 0.8rem;
+    border-radius: 5px;
+    background-color: #ff8160;
+    text-align: center;
+    text-decoration: none;
+    font-weight: 600;
+    color: white;
+    font-size: 1.2rem;
+    &:hover {
+      background-color:#ffd2c6;
+    }
+    &:active{
+      background-color:#d46c52;
+
     }
   `,
 };
@@ -18,10 +35,10 @@ function App() {
       <h1>😺고냥이 타운 생성기😺</h1>
       <S.LinkList>
         <S.LinkItem>
-          <Link to={`/color`}>색닉 생성기</Link>
+          <S.Link to={`/color`}>색깔 닉네임</S.Link>
         </S.LinkItem>
         <S.LinkItem>
-          <Link to={`/prefix`}>칭호 생성기</Link>
+          <S.Link to={`/prefix`}>커스텀 칭호</S.Link>
         </S.LinkItem>
       </S.LinkList>
     </>
